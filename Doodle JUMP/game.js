6f8d8ff813c90a5be88503e8084fbfbdc422a70c
leadonnet <lea.donnet01@gmail.com>
2024-06-04 18:31:55 +0200

    Ajout player et animation

--- Doodle JUMP/game.js
+++ Doodle JUMP/game.js
@@ -7,7 +7,32 @@ kaboom({
 });
 
 // Chargement des sprites
-loadSprite("player", "Assets/doodler-guy.png");
+//loadSprite("player", "Assets/doodler-guy.png");
+
+loadSprite("player", "Assets/playerPrincipal.png", {
+    sliceX: 23,
+    sliceY: 1,
+    anims: {
+        idle: {
+            from: 0,
+            to: 9,
+            loop: true,
+            speed: 9
+        },
+        jump: {
+            from: 10,
+            to: 13,
+            loop: true,
+            speed:3
+        },
+        runDroite: {
+            from: 14,
+            to: 22,
+            loop: true,
+            speed: 6
+        }
+    }
+})
 
 // Set the gravity acceleration (pixels per second)
 setGravity(1600);
@@ -21,7 +46,16 @@ const player = add([
     pos(center()),
     area(),
     body(),
+    scale(2),
+    //insérer un tag au moins c'est plus simple de l'appeler dans les onCollide p.ex. 
+    "player"
 ]);
+
+//pour mieux réussir à activer les mouvements 
+let animEnCours = false 
+
+//mettre comme comportement de défaut le idle
+player.play("idle")
 
 // Ensure player object is properly initialized
 if (!player) {
@@ -31,9 +65,16 @@ if (!player) {
 // Jump when space is pressed
 onKeyPress("space", () => {
     if (player.isGrounded()) {
-        player.jump();
+        //j'arrive pas encore très bien à charger l'animation quand le player saute parce que j'ai jamais travaillé avec, mais à voir
+        player.play("jump")
+        player.jump()
     }
 });
+
+onKeyRelease("space", () => {
+    player.play("idle");
+    animEnCours = false;
+}); 
 
 // Log a message when the player hits the ground
 player.onGround(() => {
@@ -41,16 +82,16 @@ player.onGround(() => {
 });
 
 // Continuous check for space key being held down to adjust jump height
-onUpdate(() => {
+/*onUpdate(() => {
     if (isKeyDown("space")) {
         if (!player.isGrounded() && player.vel.y < 0) {
             player.vel.y -= dt() * 600;
         }
     }
-});
+});*/
 
 // Move the player with arrow keys
-onKeyDown("left", () => {
+/*onKeyDown("left", () => {
     player.move(-SPEED, 0);
 });
 
@@ -64,7 +105,41 @@ onKeyDown("up", () => {
 
 onKeyDown("down", () => {
     player.move(0, SPEED);
+});*/
+
+onKeyDown("left", () => {
+    if(!animEnCours) {
+        player.play("runDroite");
+        player.flipX = true;
+        animEnCours = true;
+    }
+    player.move(-SPEED, 0)
+    if (player.pos.x < 0) {
+        player.pos.x = width()
+    }
 });
+
+onKeyRelease("left", () => {
+    player.play("idle");
+    animEnCours = false;
+}); 
+
+onKeyDown("right", () => {
+    if(!animEnCours) {
+        player.play("runDroite");
+        player.flipX = false;
+        animEnCours = true;
+    }
+    player.move(SPEED, 0)
+    if (player.pos.x > width()) {
+        player.pos.x = 0
+    }
+});
+
+onKeyRelease("right", () => {
+    player.play("idle");
+    animEnCours = false;
+}); 
 
 // Add a platform to hold the player
 add([
